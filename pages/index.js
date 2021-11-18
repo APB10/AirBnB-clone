3a30fa00cc17f3cf0ec3c93a09dd1a17962912b5
Ali P. Bouraki <ali_panahi10@hotmail.com>
2021-11-18 14:59:52 +0000

    added largCard component and footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-import Banner from '../components/Banner'
-import Header from '../components/Header'
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import LargeCard from '../components/LargeCard';
+import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
 
@@ -16,10 +19,11 @@ export default function Home({ exploreData, cardsData }) {
       <Banner />
 
       <main className="max-w-7xl mx-auto px-8 sm:px-16" >
+        
         <section className="pt-6">
           <h2 className="text-4xl font-semibold pb-5">Exlpore Nearby</h2>
 
-          {/* Data from a server - API endpoints  */}
+          {/* Data from server - API endpoints  */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {exploreData?.map(({ img, distance, location }) => (
               <SmallCard
@@ -35,12 +39,28 @@ export default function Home({ exploreData, cardsData }) {
         <section>
           <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
 
-          {cardsData.map(item => (
-            <MediumCard />
-          ))}
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ">
+            {cardsData?.map(({ img, title }) => (
+              <MediumCard
+                key={img}
+                img={img}
+                title={title}
+              />
+            ))}
+          </div>
+        </section>
 
+        <section>
+          <LargeCard
+            img="https://links.papareact.com/4cj"
+            title="The Greatest OutDoors"
+            descrption="Wishlists curated by Airbnb."
+            buttonText="Get Inspired"
+          />
         </section>
       </main>
+
+      <Footer />
 
     </div>
   )
