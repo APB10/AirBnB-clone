7f821b62998136d1d0f86d9fe412e09439e90542
Ali P. Bouraki <ali_panahi10@hotmail.com>
2021-11-19 11:42:01 +0000

    typo fixes

--- components/Banner.js
+++ components/Banner.js
@@ -7,9 +7,9 @@ function Banner() {
             layout="fill" 
             objectFit="cover" />
         <div className="absolute top-1/2 w-full text-center ">
-            <p className="text-sm sm:text-lg">Not sure where to go? Prefect.</p>
+            <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
             <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-2 
-            hover:shadow-xl active:scale-90 transition duration-150">I'm flexble</button>
+            hover:shadow-xl active:scale-90 transition duration-150">I'm flexible</button>
         </div>
         </div>
     )
